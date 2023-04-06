a83935432ce9c862f67e19b42c3735f953d72cb4
Aminu Ndakun <amin4712@gmail.com>
2023-04-06 14:45:10 +0100

    testing workflow

--- djangoexample/views.py
+++ djangoexample/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 def home(request):
-   text = """<h1>Django Example</h1>"""
+   text = """<h1>Django Example Elastic Beanstalk</h1>"""
    return HttpResponse(text)
